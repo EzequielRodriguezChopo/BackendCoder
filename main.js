d8f7595ea67c706e3b1d874302e4a93df3da2e95
Ezequiel Rodriguez <ezequielrodriguezchopo@gmail.com>
2022-12-12 23:27:53 -0300

    Desafio 2 - Manejo de archivos en JavaScript

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+const fs = require("fs");
+
 class ProductManager {
-   constructor() {
-      this.arr = [];
+   constructor(path) {
+      this.path = path;
+      if (fs.existsSync(path)) {
+         this.arr = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+      } else {
+         this.arr = [];
+      }
    }
-   addProduct(title, description, price, thumbnail, code, stock) {
+   async addProduct(title, description, price, thumbnail, code, stock) {
       let product = {
          title,
          description,
@@ -11,41 +18,88 @@ class ProductManager {
          code,
          stock,
       };
-      let finded = this.arr.find((e) => e.code === product.code);
       if (this.arr.length == 0) {
          product["id"] = this.arr.length + 1;
          this.arr.push(product);
          console.log("Producto agregado con exito");
+         await fs.promises.writeFile(this.path, JSON.stringify(this.arr, null, "\t"));
       } else {
-         if (finded) {
-            console.log("El producto ya existe");
+         product["id"] = this.arr[this.arr.length - 1].id + 1;
+         this.arr.push(product);
+         console.log("Producto agregado con exito");
+         await fs.promises.writeFile(this.path, JSON.stringify(this.arr, null, "\t"));
+      }
+   }
+   async getProducts() {
+      try {
+         if (fs.existsSync(this.path)) {
+            let productos = this.arr;
+            console.log(productos);
+            return productos;
          } else {
-            product["id"] = this.arr[this.arr.length - 1].id + 1;
-            this.arr.push(product);
-            console.log("Producto agregado con exito");
+            console.log("Archivo no existe");
+            return "Archivo no existe";
          }
+      } catch (err) {
+         throw err;
       }
-      //console.log(this.arr);
    }
-   getProducts() {
-      return this.arr;
+   async getProductById(idProduct) {
+      try {
+         if (fs.existsSync(this.path)) {
+            let producto = this.arr;
+            let productFinded = producto.find((e) => e.id == idProduct);
+            if (productFinded) {
+               console.log(productFinded);
+            } else {
+               console.log("Not Found");
+            }
+         }
+      } catch (err) {
+         throw err;
+      }
    }
-   getProductById(idProduct) {
-      let productFinded = this.arr.find((e) => e.id == idProduct);
-      if (productFinded) {
-         console.log(productFinded);
+
+   async updateProduct(idProducto, title, description, price, thumbnail, code, stock) {
+      let actualizedProduct = {
+         title,
+         description,
+         price,
+         thumbnail,
+         code,
+         stock,
+         id: idProducto,
+      };
+      let finded = this.arr.find((e) => e.id === idProducto);
+      let posicionObj = finded.id - 1;
+      this.arr[posicionObj] = actualizedProduct;
+      console.log(this.arr);
+
+      if (this.arr.length == 0) {
+         console.log("No hay productos para actualizar");
       } else {
-         console.log("Not Found");
+         console.log("Producto actualizado con exito");
+         await fs.promises.writeFile(this.path, JSON.stringify(this.arr, null, "\t"));
       }
+   }
+
+   async deleteProduct(idProducto) {
+      let finded = this.arr.find((e) => e.id === idProducto);
+      let posicionObj = finded.id - 1;
+      let removed = this.arr.splice(posicionObj, 1);
+      console.log("Producto borrado con exito");
+      await fs.promises.writeFile("./database.txt", JSON.stringify(this.arr, null, 2));
    }
 }
 
-const product1 = new ProductManager();
+const product1 = new ProductManager("database.json");
 
 product1.addProduct("Pan", "Hecho con harina", 500, "Imagen de Pan.jpeg", "ar025", 52);
 product1.addProduct("Pan", "Hecho con harina", 500, "Imagen de Pan.jpeg", "ar025", 52);
 product1.addProduct("Bizcochos", "Hecho con harina", 200, "Imagen de Bizcocho.jpeg", "ar022", 22);
 product1.addProduct("Chipa", "Hecho con harina", 300, "Imagen de Chipa.jpeg", "ar021", 22);
-//console.log(product1.getProducts());
-product1.getProductById(3);
-product1.getProductById(7);
+//console.log(product1.getProducts()); // Va a devolver todos los productos de mi archivo
+product1.getProductById(3); //Busca en el carrito un id=3
+product1.getProductById(7); //Busca un id que no existe
+product1.updateProduct(4, "Prod Act", "Hecho con harina", 300, "Imagen de Chipa.jpeg", "ar021", 22);
+//product1.deleteProduct(4); //Elimina el producto cuyo id es el 4
